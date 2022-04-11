7bc0b758d24bade24c74ada5e418f96f53f08260
nandazman <nandazmann@gmail.com>
2022-04-11 14:01:12 +0700

    update image with next image

--- components/cover-image.js
+++ components/cover-image.js
@@ -1,18 +1,20 @@
 import cn from "classnames";
+import Image from "next/image";
 import Link from "next/link";
 
 export default function CoverImage({ title, coverImage, slug }) {
   if (!coverImage) return <></>;
 
   const image = (
-    <img
+    <Image
       alt={`Cover Image for ${title}`}
       src={coverImage?.node?.sourceUrl}
       className={cn("shadow-small", {
         "hover:shadow-medium transition-shadow duration-200": slug,
       })}
-      layout={"fill"}
-      objectFit={"contain"}
+      width={678}
+      height={414}
+      layout="responsive"
     />
   );
   return (
