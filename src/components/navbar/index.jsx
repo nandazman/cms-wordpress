52914c104abd862c4b2c783a745bbe53744d0a27
nandazman <nandazmann@gmail.com>
2022-04-25 16:06:38 +0700

    add logo on desktop

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -2,9 +2,6 @@ import NavbarUserDesktop from "./user-desktop";
 import NavbarUserMobile from "./user-mobile";
 
 export default function Navigation() {
-  // useEffect(() => {
-
-  // })
   return (
     <div className="bg-light-blue text-white mb-30px">
       <NavbarUserMobile />
--- src/components/navbar/user-desktop.jsx
+++ src/components/navbar/user-desktop.jsx
@@ -1,49 +1,68 @@
 import cn from "classnames";
+import Image from "next/image";
 import { NAVIGATION_MENU } from "../../lib/constants";
+import Container from "../container";
 import LinkToMea from "../linkToMea";
 import style from "./user-desktop.module.scss";
 
 export default function NavbarUserDesktop() {
   return (
     <>
-      <nav className={cn(style.nav, "flex justify-center items-center gap-x-8 px-2.5")}>
-        {NAVIGATION_MENU.map((item, index) => {
-          return (
-            <div
-              className={cn(
-                style.navitem,
-                "text-small cursor-pointer hover:text-orange hover:font-semibold"
-              )}
-              key={index}
-            >
-              <p id={item.id}>{item.title}</p>
-              {item.subMenu ? (
-                <div className={cn(style.submenu, "gap-x-10")}>
-                  {item.subMenu.map((sub, subIndex) => {
-                    return (
-                      <div className="bg-white" key={subIndex}>
-                        <LinkToMea
-                          className="text-black font-normal hover:font-semibold"
-                          id={sub.title}
-                          to={sub.link}
-                        >
-                          {sub.title}
-                        </LinkToMea>
-                        {/* {sub.subMenu ? (
-                            <DropdownSubMenu menu={sub.subMenu} />
-                          ) : (
-                            ""
-                          )} */}
-                      </div>
-                    );
-                  })}
-                </div>
-              ) : (
-                ""
-              )}
-            </div>
-          );
-        })}
+      <nav className="relative">
+        <Container
+          className={cn(
+            style.nav,
+            "flex justify-center items-center gap-x-8 px-2.5 relative"
+          )}
+        >
+          {NAVIGATION_MENU.map((item, index) => {
+            return (
+              <div
+                className={cn(
+                  style.navitem,
+                  "text-small cursor-pointer hover:text-orange hover:font-semibold"
+                )}
+                key={index}
+              >
+                <p id={item.id}>{item.title}</p>
+                {item.subMenu ? (
+                  <div className={cn(style.submenu, "gap-x-10")}>
+                    {item.subMenu.map((sub, subIndex) => {
+                      return (
+                        <div className="bg-white" key={subIndex}>
+                          <LinkToMea
+                            className="text-black font-normal hover:font-semibold"
+                            id={sub.title}
+                            to={sub.link}
+                          >
+                            {sub.title}
+                          </LinkToMea>
+                          {/* {sub.subMenu ? (
+                              <DropdownSubMenu menu={sub.subMenu} />
+                            ) : (
+                              ""
+                            )} */}
+                        </div>
+                      );
+                    })}
+                  </div>
+                ) : (
+                  ""
+                )}
+              </div>
+            );
+          })}
+          <div className="absolute left-5 top-3">
+            <LinkToMea to="/">
+              <Image
+                width="120"
+                height="36.88"
+                src="https://storage.googleapis.com/smarketing-prod/others/logo-rebrand-ch-2_04_small.png"
+                alt="MEA logo"
+              />
+            </LinkToMea>
+          </div>
+        </Container>
       </nav>
     </>
   );
